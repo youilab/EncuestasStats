e1ea73652ef9d4caf18d6121199e7b7f80aa93cd
UrielMaD <urielmd00@gmail.com>
2021-02-10 15:49:37 -0600

    latest version

--- tequis_survey.py
+++ tequis_survey.py
@@ -7,24 +7,30 @@ edades = [int(x) for x in df['Q1'] if str(x).isnumeric()]
 fig = go.Figure()
 
 fig.add_trace(go.Histogram(
-    x = edades,
-    histnorm = 'percent',
-    name = 'Age',
-    xbins=dict( # bins used for histogram
+    x=edades,
+    histnorm='percent',
+    name='Age',
+    xbins=dict(# bins used for histogram
         start=0,
         end=70,
         size=5
     ),
     marker_color='#EB89B5',
-    opacity=0.75
+    opacity=0.75,
+    marker=dict(
+        line=dict(
+            color='black',
+            width=1
+        )
+    ),
 ))
 
 fig.update_layout(
     title_text='Edades de los participantes', # title of plot
     xaxis_title_text='Edades', # xaxis label
     yaxis_title_text='Cantidad', # yaxis label
-    bargap=0.2, # gap between bars of adjacent location coordinates
-    bargroupgap=0.1 # gap between bars of the same location coordinates
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
 )
 
 
