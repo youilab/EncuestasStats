441be9109db540670e50e3d818d5cd6683ff097b
UrielMaD <urielmd00@gmail.com>
2021-02-17 00:52:25 -0600

    Added private method for histogram settings

--- plots/histogram.py
+++ plots/histogram.py
@@ -7,18 +7,7 @@ class Histogram:
     def __init__(self, path):
         self.path = path
 
-    def hist_percents(self, ages, name, title):
-        edades = [int(x) for x in ages if str(x).isnumeric()]
-
-        bin = [x for x in range(0, 61, 5) if x > 1]
-
-        counts, bins = np.histogram(edades, bins=bin)
-
-        percentages = [round(((x / sum(counts)) * 100), 2) for x in counts]
-        bins = 0.5 * (bins[:-1] + bins[1:])
-
-        fig = go.Figure()
-
+    def _age_histogram_setup(self, fig, bins, percentages, title):
         fig.add_trace(go.Bar(
             x=bins,
             y=percentages,
@@ -33,7 +22,8 @@ class Histogram:
             ),
         ))
 
-        fig.update_traces(texttemplate=[str(x) + '%' if x > 0 else '' for x in percentages], textposition='outside')
+        fig.update_traces(texttemplate=[str(x) + '%' if x > 0 else '' for x in percentages],
+                          textposition='outside')
         fig.update_layout(
             title_text=title,
             xaxis_title_text='Edades',  # xaxis label
@@ -42,6 +32,23 @@ class Histogram:
             plot_bgcolor='rgba(0,0,0,0)',
             bargap=0.2,
         )
+
+        return fig
+
+    def hist_percents(self, ages, name, title):
+        edades = [int(x) for x in ages if str(x).isnumeric()]
+
+        bin = [x for x in range(0, 61, 5) if x > 1]
+
+        counts, bins = np.histogram(edades, bins=bin)
+
+        percentages = [round(((x / sum(counts)) * 100), 2) for x in counts]
+        bins = 0.5 * (bins[:-1] + bins[1:])
+
+        fig = go.Figure()
+
+        fig = self._age_histogram_setup(fig, bins, percentages, title)
+
         fig.write_image(self.path + name)
 
     def hist_percents_accumulated(self, ages, name, title):
@@ -60,28 +67,8 @@ class Histogram:
         bins = 0.5 * (bins[:-1] + bins[1:])
 
         fig = go.Figure()
-        fig.add_trace(go.Bar(
-            x=bins,
-            y=percentages,
-            name='Age',
-            marker_color='#EB89B5',
-            opacity=0.75,
-            marker=dict(
-                line=dict(
-                    color='black',
-                    width=1
-                )
-            ),
-        ))
 
-        fig.update_traces(texttemplate=[str(x) + '%' if x > 0 else '' for x in percentages],
-                               textposition='outside')
-        fig.update_layout(
-            title_text=title,
-            xaxis_title_text='Edades',  # xaxis label
-            yaxis_title_text='Porcentaje',  # yaxis label
-            paper_bgcolor='rgba(0,0,0,0)',
-            plot_bgcolor='rgba(0,0,0,0)',
-            bargap=0.2,
-        )
+        fig = self._age_histogram_setup(fig, bins, percentages, title)
+
         fig.write_image(self.path + name)
+
