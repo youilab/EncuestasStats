bd4686588835ea7167c391572bb8352ec9439e55
UrielMaD <urielmd00@gmail.com>
2021-03-26 21:51:08 -0600

    get_sensors_info simplified

--- ladrillera/ladrillera.py
+++ ladrillera/ladrillera.py
@@ -4,31 +4,20 @@ import pandas as pd
 def get_sensors_info(df):
     polls = ['pm25', 'pm1', 'no2', 'voc', 'pm10']
 
-    measures = {
-        'std_dev': 0,
-        'mean': 0,
-        'variance': 0,
-    }
-
-    pollutants = {
-        'pm25': measures,
-        'pm1': measures,
-        'no2': measures,
-        'voc': measures,
-        'pm10': measures,
-    }
-
     sensors_info = {}
 
     for i in df['sensor'].unique():
         sensors_info[str(i)] = {
-            'pollutants': pollutants,
+            'pollutants': {},
             'total_measurements': len(df[df['sensor'] == i]),
         }
         for _ in polls:
-            sensors_info[str(i)]['pollutants'][_]['std_dev'] = round(df[df['sensor'] == i][_].std(), 2)
-            sensors_info[str(i)]['pollutants'][_]['mean'] = round(df[df['sensor'] == i][_].mean(), 2)
-            sensors_info[str(i)]['pollutants'][_]['variance'] = round(df[df['sensor'] == i][_].var(), 2)
+            measures = {
+                'std_dev': round(df[df['sensor'] == i][_].std(), 2),
+                'mean': round(df[df['sensor'] == i][_].mean(), 2),
+                'variance': round(df[df['sensor'] == i][_].var(), 2),
+            }
+            sensors_info[str(i)]['pollutants'][_] = measures
 
     return sensors_info
 
